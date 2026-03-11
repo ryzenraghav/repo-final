cb45c3b83b2c39f19a657461ee4a5f6403235e74
TharunVel <tharunvel0507@gmail.com>
2026-03-11 19:23:43 +0530

    online-Frontend pending

--- server/server.js
+++ server/server.js
@@ -6,14 +6,21 @@ import cors from "cors";
 import passport from "passport";
 import "./passport.js";
 import authRoutes from "./authRoutes.js";
+import evaluationRoutes from "./evaluationRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(passport.initialize());
 
 app.use("/auth", authRoutes);
+app.use("/api", evaluationRoutes);
 
 app.listen(5002, () => {
-  console.log("Backend running on http://localhost:5001");
+  console.log("Backend running on http://localhost:5002");
 });
